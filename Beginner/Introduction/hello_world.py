bfa04609107da601dc3ea83c6a564accdce65c41
EngineerDilip <dilipkumar190590@gmail.com>
2024-10-06 15:01:58 +0530

    print() style in python

--- Beginner/Introduction/hello_world.py
+++ Beginner/Introduction/hello_world.py
@@ -1,1 +1,20 @@
+#Using print() function (the most common)
 print("Hello,World!!")
+
+#Using string formatter with % (old method)
+print("%s" % "Hello World!!")
+
+#Using str.format() method
+print("{}".format("Hello World!!"))
+
+#Using f-string ( only python 3.6+)
+print(f"Hello World!!")
+
+#Using repr() function
+print(repr("Hello World!!"))
+
+#Using join() with print()
+print(" ".join(["Hello", "World"]))
+
+#Using Concatenating string inside print()
+print("Hello" + "World!!")
